da62df7d5c1c9f70c19b71604f9bc26105129c74
preciz <barna.kovacs.codes@gmail.com>
2016-12-31 16:00:16 +0100

    Add InstaScraper.serialize_params function

--- lib/insta_scraper.ex
+++ lib/insta_scraper.ex
@@ -1,2 +1,18 @@
 defmodule InstaScraper do
+  @doc ~S"""
+  Serializes params
+
+  ## Examples
+      iex> InstaScraper.serialize_params(food: "15", bar: "large")
+      "?food=15&bar=large"
+
+      iex> InstaScraper.serialize_params([])
+      ""
+  """
+  def serialize_params([]), do: ""
+  def serialize_params(params) when is_list(params) do
+    "?" <> (params
+            |> Enum.map(fn {k, v} -> "#{k}=#{v}" end)
+            |> Enum.join("&"))
+  end
 end
