e63beecdc0de3ecc1e58b0fd8474ff03804fd0d1
preciz <barna.kovacs.codes@gmail.com>
2016-12-31 12:35:13 +0100

    Add HTTPoison to dependencies

--- mix.exs
+++ mix.exs
@@ -14,7 +14,9 @@ defmodule InstaScraper.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [
+      applications: [:logger, :httpoison]
+    ]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +29,6 @@ defmodule InstaScraper.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:httpoison, "0.10.0"}]
   end
 end
